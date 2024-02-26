7d1b59f6e667013cef03fadc95d379d632431ff5
Dildeep Gabrhi <dildeep.gabrhi@gmail.com>
2024-02-26 13:33:38 +0000

    new app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+        DIGITAL AGENCY WITH A PASSION FOR MARKETING
+        </p>
+        <div>
+        <span>
+        <a
+          className="App-link"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Marketing / Social 
+        </a>
+    </span>
+    <span style={{marginLeft: 20}}>
+        <a
+          className="App-link"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Technology / IOS 
+        </a>
+        </span>
+        </div>
+        <p>
+        coming soon ...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
